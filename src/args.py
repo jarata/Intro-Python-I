bd7c599b8ad50604d6885cd920c42944c408c65c
J A <jarata@gmail.com>
2019-05-09 10:44:12 -0700

    args.py 3/4 finished

--- src/args.py
+++ src/args.py
@@ -30,13 +30,18 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(sum(a)))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y = None):
+    if y is None:
+        return int(x + 1)
+    else:
+        return int(x + y)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -51,6 +56,8 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*kwargs):
+
 
 # Should print
 # key: a, value: 12
