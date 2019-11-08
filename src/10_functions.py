98ec59001fafcc79cd110509dc5f658f1d379f8e
Justin A <jarata@gmail.com>
2019-11-08 09:56:31 -0800

    function.py scopes.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,18 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(x):
+    if x % 2:
+        return False
+    elif x == 0:
+        return False
+    else:
+        return True
+    
+    
+print(is_even(12))
+print(is_even(3))
+print(is_even(0))
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +21,9 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+if is_even(num) is True:
+    print("Even!")
+else:
+    print("Odd")
 
+print(num)
